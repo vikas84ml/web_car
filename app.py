3e6af61945edb4f04dead012202c5f40a38e997e
vikas84ml <56949290+vikas84ml@users.noreply.github.com>
2020-01-27 12:14:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,34 +16,46 @@ from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
-
+# Define a flask app
 app = Flask(__name__)
 
-
+# Model saved with Keras model.save()
 MODEL_PATH = 'car_model.h5'
-# Load your trained model
-model = load_model(MODEL_PATH)
-model._make_predict_function()
 
-##For creating model
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('model_resnet.h5')
+# Load your trained model
+#model = load_model(MODEL_PATH)
+#model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+from keras.applications.resnet50 import ResNet50
+model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
 
 def model_predict(img_path, model):
     img = image.load_img(img_path, target_size=(224, 224))
+
     # Preprocessing the image
     x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
     x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
     x = preprocess_input(x, mode='caffe')
+
     preds = model.predict(x)
     return preds
 
 
-@app.route('/Image_Classification', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     # Main page
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
@@ -51,6 +63,7 @@ def upload():
         # Get the file from post request
         f = request.files['file']
 
+        # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
@@ -60,11 +73,12 @@ def upload():
         preds = model_predict(file_path, model)
 
         # Process your result for human
-        # pred_class = preds.argmax(axis=-1)
-        pred_class = decode_predictions(preds, top=1)
-        result = str(pred_class[0][0][1])
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
         return result
     return None
 
+
 if __name__ == '__main__':
-      app.run(debug=True)
+    app.run(debug=True)
