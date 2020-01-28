7fabb9eae64d79a56bebb9ca53c1a165a0c7690a
vikas84ml <56949290+vikas84ml@users.noreply.github.com>
2020-01-28 00:31:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH = 'car_model.h5'
+MODEL_PATH = 'https://github.com/vikas84ml/web_car/blob/master/car_model.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
